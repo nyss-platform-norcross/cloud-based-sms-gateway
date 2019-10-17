8a24db3f641c28702ae98605829334b52055d7f7
Nathan <nathan.pire@gmail.com>
2019-10-17 17:14:49 +0200

    making working with the API mockup

--- CloudBasedSmsGateway/CloudBasedSmsGateway/Controllers/ReceiveSmsController.cs
+++ CloudBasedSmsGateway/CloudBasedSmsGateway/Controllers/ReceiveSmsController.cs
@@ -3,7 +3,10 @@ using CloudBasedSmsGateway.Models.SmsModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
 using System;
+using System.Net.Http;
 using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 
@@ -14,13 +17,15 @@ namespace CloudBasedSmsGateway.Controllers
     public class ReceiveSmsController : ControllerBase
     {
         private readonly TwilioAppConfig twilioAppConfig;
+        private readonly HttpClient httpClient;
         public ReceiveSmsController(IOptions<TwilioAppConfig> twilioOptions)
         {
             twilioAppConfig = twilioOptions.Value;
             TwilioClient.Init(twilioAppConfig.TwilioSid, twilioAppConfig.TwilioToken);
+            httpClient = new HttpClient();
         }
         [HttpGet]
-        public ActionResult ReceiveSms([FromQuery] string From, [FromQuery] string Body, [FromQuery] string MessageSid)
+        public async Task<ActionResult> ReceiveSms([FromQuery] string From, [FromQuery] string Body, [FromQuery] string MessageSid)
         {
             var sms = new Sms
             {
@@ -28,10 +33,20 @@ namespace CloudBasedSmsGateway.Controllers
                 Text = Body,
                 BinaryText = Encoding.ASCII.GetBytes(Body),
                 MsgId = MessageSid,
-                TimeStamp = DateTime.UtcNow.ToString("YYYYMMddHHmmss")
+                TimeStamp = DateTime.UtcNow.ToString("yyyyMMddHHmmss"),
+                ApiKey = Guid.NewGuid().ToString()
             };
+            var httpContent = new StringContent(JsonSerializer.Serialize(sms), Encoding.UTF8, "application/json");
+            try
+            {
+                var res = await httpClient.PostAsync("http://0c5d9758.ngrok.io/api/SmsGateway/", httpContent);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+            }
             var response = MessageResource.Create(
-                body: "We received your message and we will give you feedback as soon as it possible",
+                body: "We received your message and we will analyse it as soon as it possible",
                 from: new Twilio.Types.PhoneNumber(twilioAppConfig.TwilioPhoneNumber),
                 to: new Twilio.Types.PhoneNumber(From)
             );
