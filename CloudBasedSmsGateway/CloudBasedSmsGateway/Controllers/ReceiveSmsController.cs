973b674f3f46b3b32ce66cce3ef988f6d59c7a16
Nathan <nathan.pire@gmail.com>
2019-10-17 16:17:27 +0200

    creating Sms model and fill it inside the receive controller

--- CloudBasedSmsGateway/CloudBasedSmsGateway/Controllers/ReceiveSmsController.cs
+++ CloudBasedSmsGateway/CloudBasedSmsGateway/Controllers/ReceiveSmsController.cs
@@ -1,11 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using CloudBasedSmsGateway.Models.AppSettings;
-using Microsoft.AspNetCore.Http;
+﻿using CloudBasedSmsGateway.Models.AppSettings;
+using CloudBasedSmsGateway.Models.SmsModel;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Options;
+using System;
+using System.Text;
 using Twilio;
 using Twilio.Rest.Api.V2010.Account;
 
@@ -22,8 +20,16 @@ namespace CloudBasedSmsGateway.Controllers
             TwilioClient.Init(twilioAppConfig.TwilioSid, twilioAppConfig.TwilioToken);
         }
         [HttpGet]
-        public ActionResult ReceiveSms([FromQuery] string From, [FromQuery] string Body)
+        public ActionResult ReceiveSms([FromQuery] string From, [FromQuery] string Body, [FromQuery] string MessageSid)
         {
+            var sms = new Sms
+            {
+                Sender = From,
+                Text = Body,
+                BinaryText = Encoding.ASCII.GetBytes(Body),
+                MsgId = MessageSid,
+                TimeStamp = DateTime.UtcNow.ToString("YYYYMMddHHmmss")
+            };
             var response = MessageResource.Create(
                 body: "We received your message and we will give you feedback as soon as it possible",
                 from: new Twilio.Types.PhoneNumber(twilioAppConfig.TwilioPhoneNumber),
